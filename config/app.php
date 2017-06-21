7c03895bbd3fef9b149ee0cbdbe8d1d2e0f442f8
Ken Lin <kenlin@kkbox.com>
2017-06-21 18:03:18 +0800

    Added enTrust facade alias

--- config/app.php
+++ config/app.php
@@ -231,6 +231,10 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
 
+        /*
+         * Kenlin added alias
+         */
+        'Entrust'   => Zizaco\Entrust\EntrustFacade::class,
     ],
 
 ];
